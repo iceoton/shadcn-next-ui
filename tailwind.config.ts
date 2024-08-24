f1e6760605c4cafcd559eb542dce827112e34992
iton.dev <iceoton@gmail.com>
2024-08-24 22:54:23 +0700

    feat: add Next-UI and test create login ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,19 @@
 import type { Config } from "tailwindcss";
+import {nextui} from "@nextui-org/theme";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
 
 const config: Config = {
   content: [
